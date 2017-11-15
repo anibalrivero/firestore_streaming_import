d8c3e171fb0cd90b3a38ab478853bdd1fb746beb
Aníbal Rivero <anibal.rivero@stcorp.nl>
2017-11-15 12:16:38 -0300

    Multiprocessing support

--- import.py
+++ import.py
@@ -2,6 +2,7 @@ import ijson.backends.yajl2_cffi as ijson
 from google.cloud import firestore
 import argparse
 import time
+import multiprocessing as mp
 
 def convert_value(value, value_type):
     if value_type == 'number':
@@ -10,14 +11,21 @@ def convert_value(value, value_type):
 
 
 def save_document(db, collection, document, data):
+    print("Saving {}".format(document))
     doc_ref = db.collection(collection).document(document)
     doc_ref.set(data)
 
+
+def multisave(db, collection, document, data):
+    p = mp.Process(target=save_document, args=(db, collection, document, data))
+    p.start()
+    return p
 
 def main(args):
     print("started at {0}".format(time.time()))
     collection = args.collection
     firedb = firestore.Client()
+    jobs = []
     with open(args.json_file, 'rb') as json_file:
         parser = ijson.parse(json_file)
         values = {}
@@ -31,10 +39,10 @@ def main(args):
             if value is not None and event not in ('map_key', ):
                 values_dict[route[-2]][route[-1]] = convert_value(value, event)
             if event == 'end_map' and len(route) == 1 and prefix is not '':
-                print("Saving {}".format(document))
-                save_document(firedb, collection, document, values_dict)
-
-
+                # save_document(firedb, collection, document, values_dict)
+                jobs.append(multisave(firedb, collection, document, values_dict))
+    for p in jobs:
+        p.join()
     print("finished at {0}".format(time.time()))
 
 if __name__ == '__main__':
