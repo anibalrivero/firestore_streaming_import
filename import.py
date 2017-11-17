7e50c86fb74d6848b38ed77bf80146a4a54837b1
Aníbal Rivero <anibal.rivero@stcorp.nl>
2017-11-17 16:54:01 -0300

    after pylint

--- import.py
+++ import.py
@@ -1,11 +1,17 @@
-import ijson.backends.yajl2_cffi as ijson
-from google.cloud import firestore
+"""
+# Firestore Streaming Import
+
+Utilizes ijson python json streaming library along with the official firestore
+library to import a large json piecemeal into Google Cloud Firestore (BETA).
+"""
+
 import argparse
 import time
-import multiprocessing as mp
 from concurrent.futures import ProcessPoolExecutor as PoolExecutor
+from google.cloud import firestore
+import ijson.backends.yajl2_cffi as ijson
 
-def convert_value(value, value_type):
+def convert_value(value: "any", value_type: str):
     """
     ijson converts all the numbers into Decimal type, this cast them to float
     so they can be inserted in firestore.
@@ -14,21 +20,30 @@ def convert_value(value, value_type):
         return float(value)
     return value
 
-def save_document2(collection, document, data):
+
+def save_document2(collection: str, document: str, data: dict):
+    """
+    Saves the data into the firebase database.
+
+    """
     # print("processing {}".format(document))
     # since the db object is not serializable, we need to open the database
     # every time so this method can be parallelized
-    db = firestore.Client() 
-    doc_ref = db.collection(collection).document(document)
+    firedb = firestore.Client()
+    doc_ref = firedb.collection(collection).document(document)
     doc_ref.set(data)
 
+
 def main(args):
+    """
+    Entry point. It parses a json file incrementally and inserts the parsed
+    objects as documents in firebase
+    """
     print("started at {0}".format(time.time()))
     collection = args.collection
     with PoolExecutor() as executor:
         with open(args.json_file, 'rb') as json_file:
             parser = ijson.parse(json_file)
-            values = {}
             for prefix, event, value in parser:
                 # print(prefix, event, value)
                 route = prefix.split(".")
@@ -37,18 +52,19 @@ def main(args):
                 if event == 'start_map':
                     values_dict = {route[-1]: {}}
                 if value is not None and event not in ('map_key', ):
-                    values_dict[route[-2]][route[-1]] = convert_value(value, event)
+                    values_dict[route[-2]][route[-1]] = convert_value(
+                        value, event)
                 if event == 'end_map' and len(route) == 1 and prefix is not '':
                     executor.submit(
                         save_document2, collection, document, values_dict)
     print("finished at {0}".format(time.time()))
 
 if __name__ == '__main__':
-    argParser = argparse.ArgumentParser(
+    arg_parser = argparse.ArgumentParser(
         description="Import a large json file into Firestore "
         "via json Streaming.")
-    argParser.add_argument(
+    arg_parser.add_argument(
         'collection', help="Specify the Firestore base collection")
-    argParser.add_argument('json_file', help="The JSON file to import.")
+    arg_parser.add_argument('json_file', help="The JSON file to import.")
 
-    main(argParser.parse_args())
+    main(arg_parser.parse_args())
